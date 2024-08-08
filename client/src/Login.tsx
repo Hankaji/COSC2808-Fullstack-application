ce79f4c1c2694fb3fc8011dc35f903851b3f5d2d
Hankaji <hankaji135@gmail.com>
2024-08-08 14:44:20 +0700

    Added logo and finish the form

--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -2,9 +2,9 @@ const Login = () => {
   return (
     <section className='flex w-svw h-svh items-center justify-center bg-primary-900'>
       <form>
-        <div className='p-12 border-solid rounded-lg shadow-lg flex flex-col gap-8 bg-primary-800'>
-          <div>
-            <img src="idk" alt="Logo here" />
+        <div className='p-12 border-solid rounded-lg shadow-lg flex flex-col gap-16 bg-primary-800'>
+          <div className="flex flex-col gap-2">
+            <img className="size-12 object-cover mx-auto" src="/logo56x56.png" alt="Logo here" />
             <p className="font-bold text-xl">SnapMate</p>
           </div>
           <div className='flex flex-col gap-1'>
@@ -22,7 +22,7 @@ const Login = () => {
               <input className="p-4 rounded-lg w-[25vw]" type="text" name="password" id="password" placeholder="Enter your Password" />
             </div>
           </div>
-          <p className="py-16"> Don't have an account? <a className="font-bold">Register</a> </p>
+          <p className="py-4"> Don't have an account? <a className="font-bold">Register</a> </p>
           <div className="p-1 flex gap-2 border-solid border-primary-500 border-2 rounded-full justify-center w-fit mx-auto">
             <button className="py-3 px-20 bg-secondary-500 rounded-full" type="button" >Sign in</button>
             <button className="py-3 px-20 rounded-full" type="button" >Register</button>
