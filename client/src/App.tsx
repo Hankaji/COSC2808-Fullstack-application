85ed677fadd6c777298980fb8f1236ae8fc2cb14
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2024-08-14 12:32:19 +0700

    chore: add prettier to format code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,48 +1,48 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./App.css";
-import LoginRegisterForm from "./LoginRegisterForm";
-import HomePage from "./pages/home/Home";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import LoginRegisterForm from './LoginRegisterForm';
+import HomePage from './pages/home/Home';
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <HomePage />,
-	},
-	{
-		path: "/login",
-		element: <LoginRegisterForm />,
-	},
-	{
-		path: "/register",
-		// element: <LoginRegisterForm />,
-	},
-	{
-		path: "/profile",
-		children: [
-			{
-				// Show people profile through this link
-				path: "/profile/:userId",
-			},
-		],
-	},
-	{
-		path: "/group",
-		children: [
-			{
-				path: "/group/:groupId",
-			},
-		],
-	},
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/login',
+    element: <LoginRegisterForm />,
+  },
+  {
+    path: '/register',
+    // element: <LoginRegisterForm />,
+  },
+  {
+    path: '/profile',
+    children: [
+      {
+        // Show people profile through this link
+        path: '/profile/:userId',
+      },
+    ],
+  },
+  {
+    path: '/group',
+    children: [
+      {
+        path: '/group/:groupId',
+      },
+    ],
+  },
 ]);
 
 function App() {
-	return (
-		<div className="App text-foreground bg-background min-h-svh">
-			{/* <LoginRegisterForm /> */}
-			{/* <HomePage /> */}
-			<RouterProvider router={router} />
-		</div>
-	);
+  return (
+    <div className="App text-foreground bg-background min-h-svh">
+      {/* <LoginRegisterForm /> */}
+      {/* <HomePage /> */}
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/HomePanel.tsx
+++ client/src/components/HomePanel.tsx
@@ -1,44 +1,44 @@
-import { useCallback, useEffect, useState } from "react";
-import PostCreationPanel from "./PostCreationPanel";
-import PostsView from "./PostsView";
+import { useCallback, useEffect, useState } from 'react';
+import PostCreationPanel from './PostCreationPanel';
+import PostsView from './PostsView';
 
 const HomePanel = () => {
-	const endpoint = "https://localhost:3000/posts"; // Placeholder, not real endpoint
+  const endpoint = 'https://localhost:3000/posts'; // Placeholder, not real endpoint
 
-	const [postData, setPostData] = useState<any>([]);
-	const [page, setPage] = useState<number>(0);
+  const [postData, setPostData] = useState<any>([]);
+  const [page, setPage] = useState<number>(0);
 
-	const getPosts = useCallback(async () => {
-		try {
-			const response = await fetch(endpoint, {
-				method: "GET",
-				headers: {
-					"content-type": "application/json;charset=UTF-8",
-				},
-			});
+  const getPosts = useCallback(async () => {
+    try {
+      const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+        },
+      });
 
-			if (!response.ok) {
-				throw new Error(`Response status: ${response.status}`);
-			}
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
 
-			const data = await response.json();
-			setPostData((prev: any) => [...prev, ...data.posts]);
-		} catch (err) {
-			console.error(err);
-		}
-	}, [page]);
+      const data = await response.json();
+      setPostData((prev: any) => [...prev, ...data.posts]);
+    } catch (err) {
+      console.error(err);
+    }
+  }, [page]);
 
-	useEffect(() => {
-		getPosts();
-	}, [getPosts]);
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
 
-	return (
-		<div className="w-full p-8">
-			{/* Post something */}
-			<PostCreationPanel />
-			<PostsView data={null} />
-		</div>
-	);
+  return (
+    <div className="w-full p-8">
+      {/* Post something */}
+      <PostCreationPanel />
+      <PostsView data={null} />
+    </div>
+  );
 };
 
 export default HomePanel;
