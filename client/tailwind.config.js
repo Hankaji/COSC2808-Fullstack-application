b927332f993b3c50b4f09018f43280108a03d7c7
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2024-08-14 12:09:37 +0700

    chore: merge with develop branch

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const config = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {
@@ -69,3 +69,5 @@ export default {
 	},
 	plugins: [],
 };
+
+export default config;
