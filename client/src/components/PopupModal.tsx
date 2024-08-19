d98e03a5c41878f6bc74f5e5b0b7b248fae3b561
Hankaji <hankaji135@gmail.com>
2024-08-20 00:26:42 +0700

    Added PopupModal component

--- client/src/components/PopupModal.tsx
+++ client/src/components/PopupModal.tsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import { FC, PropsWithChildren, ReactNode, useState } from 'react';
+import { mergeClassNames } from '../utils';
 
-const PopupModal = () => {
-  return <div>PopupModal</div>;
+interface PopupModalProps extends PropsWithChildren {
+  widthPercent?: number;
+  heightPercent?: number;
+  className?: string;
+  backdropBlur?: Number;
+  modelRender: ReactNode;
+}
+
+const PopupModal: FC<PopupModalProps> = ({
+  widthPercent = 0.5,
+  heightPercent = 0.5,
+  className,
+  backdropBlur = 0,
+  modelRender,
+  children,
+}) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <>
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+          setIsOpen(true);
+        }}
+        className={mergeClassNames(className)}
+      >
+        {children}
+      </div>
+      {/* Popup */}
+      {isOpen && (
+        <div
+          onClick={(e) => {
+            e.stopPropagation();
+            setIsOpen(false);
+          }}
+          className="fixed top-0 left-0 w-svw h-svh flex justify-center items-center"
+        >
+          <div
+            style={{
+              width: `${widthPercent * 100}%`,
+              height: `${heightPercent * 100}%`,
+            }}
+            className=""
+          >
+            {modelRender}
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default PopupModal;
