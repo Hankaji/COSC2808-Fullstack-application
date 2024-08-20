e30d5536b0b2840b7491aad950ca30e0753b4bc8
tom474 <emsignouc0407@gmail.com>
2024-08-20 11:38:51 +0700

    build: build the register function

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,28 @@
 import dotenv from "dotenv";
 import express from "express";
+import session from "express-session";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import userRouter from "./routes/userRoutes";
 import connectDB from "./models/dbConnection";
+import authRouter from "./routes/authRoutes";
 
 dotenv.config();
 const app = express();
 const port = 8080;
+
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET || "session_secret_key",
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			maxAge: 1000 * 60 * 60 * 24,
+		},
+	})
+);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -24,6 +37,7 @@ app.get("/", (req: express.Request, res: express.Response) => {
 	res.send("Hello World!");
 });
 
+app.use("/", authRouter);
 app.use("/users", userRouter);
 
 await connectDB();
