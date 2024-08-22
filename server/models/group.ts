2a75158d03c565377f864f52059af5e0b86b5d1e
tom474 <emsignouc0407@gmail.com>
2024-08-22 17:31:05 +0700

    fix: convert the image into base64

--- server/models/group.ts
+++ server/models/group.ts
@@ -19,5 +19,12 @@ const groupSchema = new mongoose.Schema(
 	{ timestamps: true }
 );
 
+groupSchema.virtual("groupImage").get(function () {
+	if (this.groupImage != null) {
+		return `data:${this.groupImage.contentType};base64,${this.groupImage.data.toString("base64")}`;
+	}
+	return undefined;
+});
+
 export const Group = mongoose.model("Group", groupSchema);
 export default Group;
