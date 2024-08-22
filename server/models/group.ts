071fb94df08a36867b11b9abe5a13bf84f602509
tom474 <emsignouc0407@gmail.com>
2024-08-22 17:03:17 +0700

    fix: modify the image data type

--- server/models/group.ts
+++ server/models/group.ts
@@ -5,8 +5,14 @@ const groupSchema = new mongoose.Schema(
 		name: { type: String, unique: true, require: true },
 		description: { type: String, required: true },
 		visibility: { type: String, required: true, enum: ["Public", "Private"] },
-		groupImage: { type: String },
-		coverImage: { type: String },
+		groupImage: {
+			data: { type: Buffer, required: true },
+			contentType: { type: String, required: true },
+		},
+		coverImage: {
+			data: { type: Buffer, required: true },
+			contentType: { type: String, required: true },
+		},
 		admins: [{ type: String }],
 		members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 	},
