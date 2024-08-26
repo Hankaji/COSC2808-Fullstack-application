56ff5f99bb57030fc58e9344d5b2bb18fc183127
Tenyama <142388224+Tenyama@users.noreply.github.com>
2024-08-26 11:27:39 +0700

    Backend/register with image (#33)
    * Register now stores profile Image
    
    * Create Group with images
    
    ---------
    
    Co-authored-by: Le Thien Son <lethienson@Les-MBP-7.STUSGS.RMIT.EDU.VN>

--- server/models/user.ts
+++ server/models/user.ts
@@ -1,31 +1,31 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-	{
-		username: { type: String, unique: true, require: true },
-		displayName: { type: String, required: true },
-		email: { type: String, required: true },
-		password: { type: String, required: true },
-		profileImage: {
-			data: { type: Buffer, required: true },
-			contentType: { type: String, required: true },
-		},
-		status: { type: String, default: "Active", enum: ["Active", "Suspended"] },
-		friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-		notifications: [
-			{
-				type: {
-					type: String,
-					required: true,
-					enum: ["User", "Group", "Post", "Comment", "Reaction"],
-				},
-				message: { type: String, required: true },
-				isRead: { type: Boolean, default: false },
-				createdAt: { type: Date, default: Date.now },
-			},
-		],
-	},
-	{ timestamps: true }
+  {
+    username: { type: String, unique: true, require: true },
+    displayName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    profileImage: {
+      data: { type: Buffer, required: true },
+      contentType: { type: String, required: true },
+    },
+    status: { type: String, default: "Active", enum: ["Active", "Suspended"] },
+    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    notifications: [
+      {
+        type: {
+          type: String,
+          required: true,
+          enum: ["User", "Group", "Post", "Comment", "Reaction"],
+        },
+        message: { type: String, required: true },
+        isRead: { type: Boolean, default: false },
+        createdAt: { type: Date, default: Date.now },
+      },
+    ],
+  },
+  { timestamps: true },
 );
 
 userSchema.virtual("virtualProfileImage").get(function () {
