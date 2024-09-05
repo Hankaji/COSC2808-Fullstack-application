6d814bdffe0dfcc50d87bf189b865794d9ff4b09
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2024-09-05 10:20:09 +0700

    chore: handle accept and reject friend requests

--- client/src/pages/friends/FriendRequestList.tsx
+++ client/src/pages/friends/FriendRequestList.tsx
@@ -1,48 +1,33 @@
-import { Check, X } from 'lucide-react';
 import { FC } from 'react';
-import { Account } from '../../types';
+import { FriendRequest } from '../../types';
 import AccInfoWithIconButtons from '../../components/AccInfoWithIconButtons';
 
-const list: Account[] = [
-  {
-    id: 'alice_on_chain',
-    username: 'alice_on_chain',
-    displayName: 'Alice',
-    imgUrl:
-      'https://preview.redd.it/lhxag30v58d31.jpg?width=640&crop=smart&auto=webp&s=bcf582e90ffb150dfd3f905fbfbe44deb30e56e6',
-  },
-  {
-    id: 'its_bob',
-    username: 'its_bob',
-    displayName: 'Bob',
-    imgUrl:
-      'https://preview.redd.it/lhxag30v58d31.jpg?width=640&crop=smart&auto=webp&s=bcf582e90ffb150dfd3f905fbfbe44deb30e56e6',
-  },
-  {
-    id: 'charliexcx',
-    username: 'charliexcx',
-    displayName: 'Charlie',
-    imgUrl:
-      'https://preview.redd.it/lhxag30v58d31.jpg?width=640&crop=smart&auto=webp&s=bcf582e90ffb150dfd3f905fbfbe44deb30e56e6',
-  },
-];
+interface FriendRequestListProps {
+  requests: FriendRequest[];
+  onAcceptReq: (req: FriendRequest) => void;
+  onRejectReq: (req: FriendRequest) => void;
+}
 
-const FriendRequestList: FC = () => {
+const FriendRequestList: FC<FriendRequestListProps> = ({
+  requests,
+  onAcceptReq,
+  onRejectReq,
+}) => {
   return (
     <div className="flex flex-col h-[calc(100vh-180px)]">
       <div className="flex-grow overflow-y-auto mt-2 pr-3">
-        {list.map((acc) => (
+        {requests.map((req) => (
           <AccInfoWithIconButtons
-            key={acc.id}
-            data={acc}
+            key={req.id}
+            data={req.acc}
             buttons={[
               {
                 type: 'accept',
-                onClick: () => {},
+                onClick: () => onAcceptReq(req),
               },
               {
                 type: 'reject',
-                onClick: () => {},
+                onClick: () => onRejectReq(req),
               },
             ]}
           />
