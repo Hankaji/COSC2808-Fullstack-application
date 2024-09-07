f89e3116cd200fe3b39b751fc36fdca038a1d937
Hankaji <hankaji135@gmail.com>
2024-09-07 13:59:38 +0700

    Fixed CompactedGroup display incorrectly

--- client/src/components/CompactedGroup.tsx
+++ client/src/components/CompactedGroup.tsx
@@ -16,45 +16,45 @@ const CompactedGroup: FC<GroupInfoProps> = ({ data, button }) => {
   const buttonCmp = !button
     ? null
     : (() => {
-        const handleOnClick = (
-          e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-        ) => {
-          e.preventDefault();
-          button.onClick?.();
-        };
+      const handleOnClick = (
+        e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+      ) => {
+        e.preventDefault();
+        button.onClick?.();
+      };
 
-        switch (button.type) {
-          case 'toRequest':
-            return (
-              <button
-                onClick={handleOnClick}
-                className="rounded-full p-1.5 bg-blue-100 hover:bg-blue-200"
-              >
-                <Plus size={16} className="stroke-blue-900" />
-              </button>
-            );
-          case 'requestSent':
-            return (
-              <button className="rounded-full p-1.5 bg-gray-100 cursor-default">
-                <Check size={16} className="stroke-gray-900" />
-              </button>
-            );
-          case 'member':
-            return (
-              <button className="rounded-full p-1.5 bg-green-100">
-                <UserRoundCheck size={16} className="stroke-green-900" />
-              </button>
-            );
-        }
-      })();
+      switch (button.type) {
+        case 'toRequest':
+          return (
+            <button
+              onClick={handleOnClick}
+              className="rounded-full p-1.5 bg-blue-100 hover:bg-blue-200"
+            >
+              <Plus size={16} className="stroke-blue-900" />
+            </button>
+          );
+        case 'requestSent':
+          return (
+            <button className="rounded-full p-1.5 bg-gray-100 cursor-default">
+              <Check size={16} className="stroke-gray-900" />
+            </button>
+          );
+        case 'member':
+          return (
+            <button className="rounded-full p-1.5 bg-green-100">
+              <UserRoundCheck size={16} className="stroke-green-900" />
+            </button>
+          );
+      }
+    })();
 
   return (
     <Link
       to={`/groups/${data.id}`}
       className="rounded-md flex justify-between items-center px-2 py-3 hover:bg-secondary/50 cursor-pointer transition-colors"
     >
-      <div className="flex gap-2 justify-start">
-        <div className="size-12 overflow-hidden rounded-full bg-gray-500">
+      <div className="flex gap-2 items-center justify-start">
+        <div className="min-h-12 min-w-12 size-12 overflow-hidden rounded-full bg-gray-500">
           {data.groupImage && (
             <img
               className="object-cover size-12"
