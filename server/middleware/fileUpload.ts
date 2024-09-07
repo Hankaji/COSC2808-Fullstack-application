bbfa30eb2342fec4784989a406db0b00ab69ac60
vutuanlinh2k2 <69841784+vutuanlinh2k2@users.noreply.github.com>
2024-09-07 11:12:50 +0700

    chore: format code (#79)

--- server/middleware/fileUpload.ts
+++ server/middleware/fileUpload.ts
@@ -1,19 +1,19 @@
-import multer from "multer";
+import multer from 'multer';
 
 const storage = multer.memoryStorage();
 const fileUpload = multer({
-	storage: storage,
-	limits: {
-		// Limit file size to 2MB
-		fileSize: 2 * 1024 * 1024,
-	},
-	fileFilter: (req, file, cb) => {
-		if (file.mimetype.startsWith("image/")) {
-			cb(null, true);
-		} else {
-			cb(new Error("Invalid file type, only images are allowed!"));
-		}
-	},
+  storage: storage,
+  limits: {
+    // Limit file size to 2MB
+    fileSize: 2 * 1024 * 1024,
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith('image/')) {
+      cb(null, true);
+    } else {
+      cb(new Error('Invalid file type, only images are allowed!'));
+    }
+  },
 });
 
 export default fileUpload;
--- server/routes/postRoutes.ts
+++ server/routes/postRoutes.ts
@@ -1,6 +1,6 @@
-import express from "express";
-import { isAuthenticated } from "../middleware/authentication";
-import fileUpload from "../middleware/fileUpload";
+import express from 'express';
+import { isAuthenticated } from '../middleware/authentication';
+import fileUpload from '../middleware/fileUpload';
 import {
   getPosts,
   getUserPosts,
@@ -19,49 +19,49 @@ import {
   addReactionToComment,
   editReactionOnComment,
   deleteReactionFromComment,
-} from "../controllers/postController";
+} from '../controllers/postController';
 
 const postRouter = express.Router();
 
 // Post routes
-postRouter.get("/", isAuthenticated, getPosts);
-postRouter.get("/user/:id", isAuthenticated, getUserPosts);
-postRouter.get("/group/:id", isAuthenticated, getGroupPosts);
-postRouter.get("/:id", isAuthenticated, getPostById);
-postRouter.get("/:id/history", isAuthenticated, getPostHistoryById);
-postRouter.post("/", isAuthenticated, fileUpload.array("images"), createPost);
-postRouter.patch("/:id", isAuthenticated, fileUpload.array("images"), editPost);
-postRouter.delete("/:id", isAuthenticated, deletePost);
+postRouter.get('/', isAuthenticated, getPosts);
+postRouter.get('/user/:id', isAuthenticated, getUserPosts);
+postRouter.get('/group/:id', isAuthenticated, getGroupPosts);
+postRouter.get('/:id', isAuthenticated, getPostById);
+postRouter.get('/:id/history', isAuthenticated, getPostHistoryById);
+postRouter.post('/', isAuthenticated, fileUpload.array('images'), createPost);
+postRouter.patch('/:id', isAuthenticated, fileUpload.array('images'), editPost);
+postRouter.delete('/:id', isAuthenticated, deletePost);
 
 // Comment routes
-postRouter.post("/:id/comment", isAuthenticated, addCommentToPost);
+postRouter.post('/:id/comment', isAuthenticated, addCommentToPost);
 postRouter.patch(
-  "/:id/comment/:comment_id",
+  '/:id/comment/:comment_id',
   isAuthenticated,
   editCommentOnPost,
 );
 postRouter.delete(
-  "/:id/comment/:comment_id",
+  '/:id/comment/:comment_id',
   isAuthenticated,
   deleteCommentFromPost,
 );
 
 // Reaction routes
-postRouter.post("/:id/reaction", isAuthenticated, addReactionToPost);
-postRouter.patch("/:id/reaction/", isAuthenticated, editReactionOnPost);
-postRouter.delete("/:id/reaction", isAuthenticated, deleteReactionFromPost);
+postRouter.post('/:id/reaction', isAuthenticated, addReactionToPost);
+postRouter.patch('/:id/reaction/', isAuthenticated, editReactionOnPost);
+postRouter.delete('/:id/reaction', isAuthenticated, deleteReactionFromPost);
 postRouter.post(
-  "/:id/comment/:comment_id/reaction",
+  '/:id/comment/:comment_id/reaction',
   isAuthenticated,
   addReactionToComment,
 );
 postRouter.patch(
-  "/:id/comment/:comment_id/reaction",
+  '/:id/comment/:comment_id/reaction',
   isAuthenticated,
   editReactionOnComment,
 );
 postRouter.delete(
-  "/:id/comment/:comment_id/reaction",
+  '/:id/comment/:comment_id/reaction',
   isAuthenticated,
   deleteReactionFromComment,
 );
