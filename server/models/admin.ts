087f07aaa0af82d08836b956c6b88105d306149e
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2024-09-07 11:11:06 +0700

    chore: format code

--- server/models/admin.ts
+++ server/models/admin.ts
@@ -1,12 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const adminSchema = new mongoose.Schema(
-	{
-		username: { type: String, unique: true, require: true },
-		password: { type: String, required: true },
-	},
-	{ timestamps: true }
+  {
+    username: { type: String, unique: true, require: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true },
 );
 
-export const Admin = mongoose.model("Admin", adminSchema);
+export const Admin = mongoose.model('Admin', adminSchema);
 export default Admin;
